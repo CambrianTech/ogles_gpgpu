f67f89bf83e39b234e8e625aa49c471d93f821bf
Joel Teply <joelteply@yahoo.com>
2016-05-08 21:29:55 -0500

    neon support, android 19

--- examples/android/OGVideoProcDroid/jni/Application.mk
+++ examples/android/OGVideoProcDroid/jni/Application.mk
@@ -3,7 +3,9 @@ APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions
 
 # Build all available ABIs
-#APP_ABI := all
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := all
+#APP_ABI := armeabi-v7a arm64-v8a
+
+LOCAL_ARM_NEON  := true
 # What platform (API level) to build against
-APP_PLATFORM := android-14
+APP_PLATFORM := android-19
