4ae561afbcd0242faf5adfa536ce106b0f116d42
Joel Teply <joelteply@yahoo.com>
2016-05-18 09:14:50 -0500

    turn off logging

--- ogles_gpgpu/platform/android/macros.h
+++ ogles_gpgpu/platform/android/macros.h
@@ -15,7 +15,7 @@
 #define OG_TO_STR_(x) #x
 #define OG_TO_STR(x) OG_TO_STR_(x)
 
-#if 1
+#if DEBUG
 
 #define OG_LOGINF(class, args...)  __android_log_write(ANDROID_LOG_INFO, "ogles_gpgpu", class); __android_log_write(ANDROID_LOG_INFO, "ogles_gpgpu", __FUNCTION__); __android_log_print(ANDROID_LOG_INFO, "ogles_gpgpu", args)
 #else
